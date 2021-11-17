79d85d0302525ba7f0c1efbf1c0a50a23286cf4f
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-11-17 13:04:01 +0530

    Update new_join_fn.py

--- bot/plugins/new_join_fn.py
+++ bot/plugins/new_join_fn.py
@@ -71,19 +71,4 @@ async def help_message_f(client, message):
     ## Force Sub ##
     await message.reply_text(
         Localisation.HELP_MESSAGE,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('Updates Channel', url='https://t.me/Discovery_Updates')
-                ],
-                [
-                    InlineKeyboardButton('Support Group', url='https://t.me/linux_repo')
-                ],
-                [
-                    InlineKeyboardButton('Developer', url='https://t.me/AbirHasan2005'), # Bloody Thief, Don't Become a Developer by Stealing other's Codes & Hard Works!
-                    InlineKeyboardButton('Source Code', url='https://github.com/AbirHasan2005/VideoCompress') # Must Give us Credits!
-                ]
-            ]
-        ),
-        quote=True
     )
